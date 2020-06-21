dbd311e0e45eccd6c5d4d4b5b7a9177cbc35e311
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-21 00:48:45 -0400

    add `rem`, also defines `mod`

--- src/Quadmath.jl
+++ src/Quadmath.jl
@@ -11,7 +11,7 @@ import Base: (*), +, -, /,  <, <=, ==, ^, convert,
           acos, acosh, asin, asinh, atan, atanh, cosh, cos, sincos,
           exp, expm1, log, log2, log10, log1p, sin, sinh, sqrt,
           tan, tanh,
-          ceil, floor, trunc, round, fma, decompose, rem, mod,
+          ceil, floor, trunc, round, fma, decompose, rem,
           copysign, flipsign, max, min, hypot, abs,
           ldexp, frexp, modf, nextfloat, typemax, typemin, eps,
           isinf, isnan, isfinite, isinteger,
@@ -346,7 +346,6 @@ atan(x::Float128, y::Float128) =
 sincos(x::Float128) = (sin(x), cos(x))
 
 rem(x::Float128, y::Float128) = Float128(@ccall(libquadmath.fmodq(x::Cfloat128, y::Cfloat128)::Cfloat128))
-mod(x::Float128, y::Float128) = signbit(x) === signbit(y) ? rem(x,y) : y + rem(x,y)
 
 ## misc
 @static if !Sys.iswindows()
